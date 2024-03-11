17506eb12aaf2990a35221d564aadbabca15f892
admin <jonathan_fernandes@bjsmicschool.com>
2024-03-11 12:04:04 +0800

    Init repo with unit 2

---
+++ Unit 2 - Intro to JavaScript/app.js
@@ -0,0 +1,58 @@
+// // var myName;
+
+// // myName = "Jonathan";
+
+// let myName = "Nolan"
+
+// myName = "Jack"
+
+// // alert("Hello " + myName);
+
+// console.log(myName)
+
+// let isAdriaanSilly = true
+
+// let myMentalAge = 2
+
+// let moneyInHenrysPocket = 1.3
+
+// console.log(typeof(isAdriaanSilly));
+// console.log(typeof(moneyInHenrysPocket));
+
+// let myName = ""
+
+// if (myName) { 
+//     console.log("This was the first block")
+// } else if (myName == 7) {
+//     console.log("This was the second block")
+// } else {
+//     console.log("This was the third block")
+// }
+
+// // function greeting() {
+// //     console.log("Hello")
+// // }
+
+// let greeting = name => {
+//     console.log("Hello " + name)
+// }
+
+// greeting("Jon")
+
+// var myStudents = ["Richard", {name: "Adriaan"}, "Nolan"]
+// myStudents.push("Henry")
+
+// console.log(myStudents[1].name);
+
+// var myFavoriteStudent = {
+//     name: "Henry",
+//     age: 17,
+//     greeting: () => {console.log("Hello World");}
+// }
+
+// myFavoriteStudent.greeting()
+// console.log(typeof(myFavoriteStudent));
+
+let myName = prompt("What is your name?")
+
+console.log(myName)
